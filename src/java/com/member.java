74addaffb1961d70c552f6c732b96ca51bdbd77e
AshRichards <33025682+AshRichards@users.noreply.github.com>
2017-11-20 15:28:02 +0000

    updateMEM

--- src/java/com/member.java
+++ src/java/com/member.java
@@ -37,47 +37,47 @@ public class member extends HttpServlet {
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         response.setContentType("text/html;");
-        
+
         ServletContext sc = request.getServletContext();
-            Connection con = (Connection)sc.getAttribute("connection");
-            JdbcUserQry jdbc = new JdbcUserQry();
-            jdbc.connect(con);
-            
-            String uname = request.getParameter("username");
-            String pword = request.getParameter("password");
-            String qry = "Select * from ROOT.users where USERS.\"id\"='"+uname+"' and USERS.\"password\"='"+pword+"'";
-            boolean res;
-            try {
-                res = jdbc.login(qry);
+        Connection con = (Connection) sc.getAttribute("connection");
+        JdbcUserQry jdbc = new JdbcUserQry();
+        jdbc.connect(con);
+
+        String uname = request.getParameter("username");
+        String bal = "0";
+        String qry = "Select balance from ROOT.members where members.\"id\"='" + uname;
+        String qryClaim = "Select * from ROOT.claims where claims.\"mem_id\"='" + uname;
+        
+        String res,res2;
+        try {
+            res = jdbc.retrieve(qry);
+            res2 = jdbc.retrieve(qryClaim);
 //                String type = jdbc.userType(uname);
-                PrintWriter out = response.getWriter();
-                if(res==true){
-                    request.setAttribute("user", uname);
+            PrintWriter out = response.getWriter();
+            
+                request.setAttribute("user", uname);
+                request.setAttribute("balance", res);
+                request.setAttribute("claims", res2);
 //                    request.setAttribute("type", type);
-//                    RequestDispatcher view = request.getRequestDispatcher("memberDashboard.jsp");
-//                    view.forward(request,response);
-                } else {
-                    out.println("Username or Password is Incorrect");
-                }
-            } catch (SQLException ex) {
-                Logger.getLogger(login.class.getName()).log(Level.SEVERE, null, ex);
-            }
+                    RequestDispatcher view = request.getRequestDispatcher("memberDashboard.jsp");
+                    view.forward(request,response);
             
-            
-        
-        
-//        try (PrintWriter out = response.getWriter()) {
-//            /* TODO output your page here. You may use following sample code. */
-//            out.println("<!DOCTYPE html>");
-//            out.println("<html>");
-//            out.println("<head>");
-//            out.println("<title>Servlet member</title>");            
-//            out.println("</head>");
-//            out.println("<body>");
-//            out.println("<h1>Servlet member at " + request.getContextPath() + "</h1>");
-//            out.println("</body>");
-//            out.println("</html>");
-//        }
+        } catch (SQLException ex) {
+            Logger.getLogger(member.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet member</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet member at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
